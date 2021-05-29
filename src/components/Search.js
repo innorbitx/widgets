3b84f8ba2e72254a3d07417303fd10c3694fc788
Jose Luis Garza <josegarzaramos@gmail.com>
2021-05-29 00:20:10 -0500

    Added timeout Search

--- src/components/Search.js
+++ src/components/Search.js
@@ -20,11 +20,19 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    setTimeout(() => {
-      if (term) {
-        search();
-      }
-    }, 500);
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 1000);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
   }, [term]);
 
   const renderedResults = results.map((result) => {
