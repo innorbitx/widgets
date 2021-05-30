6ccac64617854bae2ea90ca1d6242fe434192de6
Jose Luis Garza <josegarzaramos@gmail.com>
2021-05-29 22:54:16 -0500

    Added Dropdown toggle

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import Accordion from './components/Accordion';
-import Search from './components/Search';
+import React, { useState } from 'react';
+// import Accordion from './components/Accordion';
+// import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 
-const items = [
-  {
-    title: 'What is React?',
-    content: 'React is a front end javascript framework',
-  },
-  {
-    title: 'Why React?',
-    content: 'React is a favorite JS library among engineers',
-  },
-  {
-    title: 'How do you use React?',
-    content: 'You use React by creating components',
-  },
-];
+// const items = [
+//   {
+//     title: 'What is React?',
+//     content: 'React is a front end javascript framework',
+//   },
+//   {
+//     title: 'Why React?',
+//     content: 'React is a favorite JS library among engineers',
+//   },
+//   {
+//     title: 'How do you use React?',
+//     content: 'You use React by creating components',
+//   },
+// ];
 
 const options = [
   {
@@ -34,11 +34,23 @@ const options = [
 ];
 
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+
   return (
     <div>
-      <Accordion items={items} />
-      <Search />
-      <Dropdown options={options} />
+      {/* <Accordion items={items} />
+      <Search /> */}
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle Dropdown
+      </button>
+      {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      ) : null}
     </div>
   );
 };
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,25 +1,65 @@
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { BiCaretDown } from 'react-icons/bi';
 
-const Dropdown = ({ options }) => {
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    const onBodyClick = (event) => {
+      if (ref.current.contains(event.target)) {
+        return;
+      }
+      setOpen(false);
+    };
+    document.body.addEventListener('click', onBodyClick, { capture: true });
+
+    return () => {
+      document.body.removeEventListener('click', onBodyClick, {
+        capture: true,
+      });
+    };
+  }, []);
+
   const renderedOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null;
+    }
+
     return (
-      <div key={option.value} className="item">
+      <div
+        key={option.value}
+        className="hover:bg-blue-500"
+        onClick={() => onSelectedChange(option)}
+      >
         {option.label}
       </div>
     );
   });
 
   return (
-    <div className="">
-      <input type="text" className="form-input rounded-xl text-blue-500" />
+    <div ref={ref} className="p-3">
+      {/* <input type="text" className="form-input rounded-xl text-blue-500" /> */}
       <label className="block text-gray-700 text-sm font-bold mb-2">
         Select a Color
       </label>
-      <div className="">
-        <BiCaretDown className="fill-current text-blue-600" />
-        <div className="">Select Color</div>
-        <div className="">{renderedOptions}</div>
+      <div className="shadow-lg block rounded-lg">
+        <div
+          onClick={() => setOpen(!open)}
+          className="p-4 flex justify-between"
+        >
+          <div className="">{selected.label}</div>
+          <BiCaretDown className="fill-current text-blue-600" />
+        </div>
+        <div
+          className={`p-4 pt-0 divide-y divide-blue-300 ${
+            open ? 'block' : 'hidden'
+          }`}
+          onClick={() => setOpen(false)}
+        >
+          {renderedOptions}
+        </div>
       </div>
     </div>
   );
@@ -27,6 +67,8 @@ const Dropdown = ({ options }) => {
 
 Dropdown.propTypes = {
   options: PropTypes.array,
+  selected: PropTypes.any,
+  onSelectedChange: PropTypes.any,
 };
 
 export default Dropdown;
